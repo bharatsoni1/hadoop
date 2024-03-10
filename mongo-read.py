acc42647adb3396d28b4db0447fdba5d41cd2b17
Bharat Dinesh Soni <63162235+bharatsoni1@users.noreply.github.com>
2024-03-10 23:15:51 +0530

    Add files via upload

---
+++ mongo-read.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+from pyspark.sql import functions
+
+def parseInput(line):
+    fields = line.split("|")
+    return Row(user_id = int(fields[0])), age = int(fields[1]), gender = fields[2], occupation = fields[3], zip = fields[4])
+    
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("MongoDBIntegration").getOrCreate()
+    
+    lines = spark.sparkContext.TextFile("hdfs:///user/maria_dev/mobgo_db/movies.user")
+    
+    user = lines.map(parseInput)
+    
+    userDataset = spark.createDataFrame(user)
+    
+    userDataset.write.format("com.mongodb.spark.sql.DefaultSource").option("uri", "mongodb://127.0.0.1/moviesdata.users")\
+    .mode("append").save()
+
